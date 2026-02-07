cce34284e8c24faa2f45c9a78a854ea6d1930b52
Stepan <sguralmen@gmail.com>
2026-02-06 22:20:40 -0330

    Adding embButton.h. First version of this library

---
+++ embButton.h
@@ -0,0 +1,127 @@
+#ifndef __EMBBUTTON_H__
+#define __EMBBUTTON_H__
+
+//EmbButton by st3p40
+//06.02.26
+
+#ifdef EmbBtnOneMillisFunc
+#define _EMBBTNMILLISFUNC embButtonMillisFunc
+unsigned long (*embButtonMillisFunc) ();
+#endif
+
+typedef enum embButtonState {await, pressed, hold, released};
+
+typedef struct embButton_t
+{
+    char isClicked;
+    char isReleased;
+    char isHold;
+
+    char _lastState;
+    
+    char clicks;
+    char endClicks;
+    char clicksWithHold;
+
+    unsigned int _lastChange;
+
+    unsigned int timer;
+    unsigned int holdTime;
+    unsigned int releaseTime;
+    unsigned int debounceTime;
+
+    embButtonState state;
+    
+    char (*buttonCheck) ();
+#ifndef EmbBtnOneMillisFunc
+#define _EMBBTNMILLISFUNC btn->millisFunc
+    unsigned long (*millisFunc) ();
+#endif
+} embButton_t;
+
+void embButtonTick(embButton_t *btn)
+{
+      btn->isClicked = 0;
+      btn->isHold = 0;
+      btn->isReleased = 0;
+      btn->endClicks = 0;
+
+      char _pressed = 0;
+      unsigned long t = _EMBBTNMILLISFUNC();
+      char reading = btn->buttonCheck();
+      if (reading != btn->_lastState)
+      {
+          btn->_lastChange = t;
+      }
+      if (t - btn->_lastChange > btn->debounceTime)
+      {
+        _pressed = !reading;
+        if (_pressed)
+        {
+          switch (btn->state)
+          {
+            case await:
+              btn->state = pressed;
+              btn->timer = t;
+              btn->clicks = 1;
+              btn->isClicked = 1;
+              break;
+
+            case pressed:
+              if (t - btn->timer >= btn->holdTime)
+              {
+                btn->state = hold;
+                btn->isHold = 1;
+              }
+            break;
+
+          case released:
+            btn->state = pressed;
+            btn->timer = t;
+            btn->clicks++;
+            btn->isClicked = 1;
+          break;
+         }
+      }
+      else
+      {
+        switch (btn->state)
+        {
+          case pressed:  
+            if (btn->clicks == 15)
+            {
+              btn->state = await;
+              btn->endClicks = btn->clicks;
+              btn->clicks = 0;
+            }
+            else
+            { 
+              btn->state = released; 
+              btn->timer = t;
+              btn->isReleased = 1;
+          break;
+
+          case hold:
+            btn->state = await;
+            btn->timer = t - btn->timer;
+            btn->clicks = 0; btn->isReleased = 1;
+          break;
+
+          case released:
+            if (t - btn->timer >= btn->releaseTime)
+            {
+              btn->state = await;
+              btn->timer = t;
+              btn->endClicks = btn->clicks;
+              btn->clicks = 0;
+            }
+          break;
+        }
+      }
+    }    
+  }
+  btn->_lastState = reading;
+};
+
+
+#endif
