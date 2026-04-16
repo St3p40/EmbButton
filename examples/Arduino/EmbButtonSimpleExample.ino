16e1d800fb828fd4694ae6825dc35753116c1fd0
Stepan <sguralmen@gmail.com>
2026-04-16 09:33:01 -0230

    EmbButton rework (#1)
    * embButton rework
    
    - embButton stats variables was moved to own struct for better resetting stats or copypasting buttons when need
    - defines are renamed to be more readable and satisfy codestyle
    - millis is global for default + btnread and millis can be defined as you need
    - action callbacks now use btn's pointer to get stat you need
    - handling multiple buttons was reworked
    - added defines to read button easely
    - added ability to set millis and btn_read (Callback, Pointer, Variable...)
    - refactoring
    - added implementation define for handling multiple definitions problem

--- examples/Arduino/EmbButtonSimpleExample.ino
+++ examples/Arduino/EmbButtonSimpleExample.ino
@@ -1,46 +1,54 @@
+#define EMBBTN_ENABLE_STEP
+#define EMBBTN_FUNCTIONS_IMPLEMENTATION
 #include "embButton.h"
 char btn_read() {return digitalRead(2);}
 embButton_t btn;
 
 void setup() {
+  embButtonMillis = millis;
   pinMode(2, INPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(13, OUTPUT);
   btn.debounceTime=25;
   btn.releaseTime=250;
+  btn.stepTime=250;
   btn.holdTime=500;
-  btn.millisFunc=millis;
   btn.buttonCheck=btn_read;
   Serial.begin(9600);
 }
 
 void loop() {
   embButtonTick(&btn);
-  if (btn.state > 0) //Pressed
+  if (btn.s.state & 0b10) //Pressed
     digitalWrite(13, 1);
   else
     digitalWrite(13, 0);
 
-  if (btn.state == EMB_BTN_STATE_PRESSED)
+  if (btn.s.state == EMBBTN_STATE_PRS)
     digitalWrite(3, 1);
   else
     digitalWrite(3, 0);
 
-    if (btn.state == EMB_BTN_STATE_HELD)
+    if (btn.s.state == EMBBTN_STATE_HLD)
     digitalWrite(4, 1);
   else
     digitalWrite(4, 0);
 
-  if (btn.isReleased)
+  if (btn.s.released)
   {
     //Serial.print((btn.lastPressType)?"-":"."); // Morse code output
-    Serial.print((btn.lastPressType)?"Hold: ":"Clicked: ");
-    Serial.println(millis()-btn.timer); //Press time
+    Serial.print((btn.s.lastPressType)?"Hold: ":"Clicked: ");
+    Serial.println(millis()-btn.s.timer); //Press time
   }
 
-  if (btn.endClicks){
+    if (btn.s.step){
     //Serial.print(" "); // Morse code output
-    Serial.print("Clicks strikes end. Clicks: " + String(btn.clicks) + "\n");
+    Serial.print("Step\n");
+  }
+
+  if (btn.s.endClicking){
+    //Serial.print(" "); // Morse code output
+    Serial.print("Clicks strikes end. Clicks: " + String(btn.s.clicks) + "\n");
   }
 }
--- examples/Linux/embButton_demo.c
+++ examples/Linux/embButton_demo.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <sys/time.h>
 #include <stdlib.h>
+#define EMBBTN_FUNCTIONS_IMPLEMENTATION
 #include "../../src/embButton.h"
 
 #define GREEN "\033[92m"
@@ -36,7 +37,7 @@ unsigned long millis(void)
                            useconds / 1000UL);
 }
 
-char btn_read(void) {
+unsigned char btn_read(void) {
     return key_down;
 }
 
@@ -50,10 +51,10 @@ int main(int argc, char **argv)
 
     const char *path = argv[1];
     embButton_t btn;
+    embButtonMillis = millis;
     btn.debounceTime=100;
     btn.releaseTime=500;
     btn.holdTime=1000;
-    btn.millisFunc=millis;
     btn.buttonCheck=btn_read;
     int fd = open(path, O_RDONLY);
     if (fd < 0) {
@@ -78,20 +79,20 @@ int main(int argc, char **argv)
         printf("\033[KTime: %s%lu ms%s\n", GREEN, millis(), RESET);
 
 printf("State:  %s  %s  %s  %s\n",
-       (btn.state == 0)  ? GREEN "AWAIT"    RESET : RED "Await" RESET,
-       (btn.state == -1) ? GREEN "RELEASED" RESET : RED "Released" RESET,
-       (btn.state == 1)  ? GREEN "PRESSED"  RESET : RED "Pressed" RESET,
-       (btn.state == 2)  ? GREEN "HOLD"     RESET : RED "Hold" RESET);
+       (btn.s.state == 0)  ? GREEN "AWAIT"    RESET : RED "Await" RESET,
+       (btn.s.state == 1) ? GREEN "RELEASED" RESET : RED "Released" RESET,
+       (btn.s.state == 2)  ? GREEN "PRESSED"  RESET : RED "Pressed" RESET,
+       (btn.s.state == 3)  ? GREEN "HOLD"     RESET : RED "Hold" RESET);
 
 printf("Flags:  %s  %s  %s  %s\n",
-       (btn.endClicks)  ? GREEN "ENDCLICKS" RESET : RED "Endclicks" RESET,
-       (btn.isReleased) ? GREEN "RELEASED"  RESET : RED "Released" RESET,
-       (btn.isClicked)  ? GREEN "CLICKED"   RESET : RED "Clicked" RESET,
-       (btn.isHold)     ? GREEN "HOLD"      RESET : RED "Hold" RESET);
+       (btn.s.endClicking)  ? GREEN "ENDCLICKS" RESET : RED "Endclicks" RESET,
+       (btn.s.released) ? GREEN "RELEASED"  RESET : RED "Released" RESET,
+       (btn.s.clicked)  ? GREEN "CLICKED"   RESET : RED "Clicked" RESET,
+       (btn.s.held)     ? GREEN "HOLD"      RESET : RED "Hold" RESET);
 
-        printf("\033[KClicks: %s%d %s%s\n", (btn.endClicks)? RED : GREEN, btn.clicks, (btn.lastPressType)? "H" : "C", RESET);
-        printf("\033[KPress time: %s%ld%s\n", (btn.isReleased)? RED : GREEN,((btn.state > 0 && !btn.isClicked) ^ btn.isReleased)?(millis()-btn.timer):0,RESET);
-        printf("\033[KIdle time: %s%ld%s\n", (btn.isClicked)? RED : GREEN,((btn.state <= 0 && !btn.isReleased) ^ btn.isClicked)?(millis()-btn.timer):0,RESET);
+        printf("\033[KClicks: %s%d %s%s\n", (btn.s.endClicking)? RED : GREEN, btn.s.clicks, (btn.s.lastPressType)? "H" : "C", RESET);
+        printf("\033[KPress time: %s%ld%s\n", (btn.s.released)? RED : GREEN,((btn.s.state > 1 && !btn.s.clicked) ^ btn.s.released)?(millis()-btn.s.timer):0,RESET);
+        printf("\033[KIdle time: %s%ld%s\n", (btn.s.clicked)? RED : GREEN,((btn.s.state <= 1 && !btn.s.released) ^ btn.s.clicked)?(millis()-btn.s.timer):0,RESET);
         fflush(stdout);
         usleep(100000);
     }
